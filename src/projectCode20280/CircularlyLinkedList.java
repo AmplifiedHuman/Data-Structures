d7a346b8e75e26f0738528a47dfcf5e66d354f57
AmplifiedHuman <amplifiedhuman@gmail.com>
2020-01-30 23:26:05 +0000

    Added methods for SinglyLinkedList

--- src/projectCode20280/CircularlyLinkedList.java
+++ src/projectCode20280/CircularlyLinkedList.java
@@ -4,102 +4,101 @@ import java.util.Iterator;
 
 public class CircularlyLinkedList<E> implements List<E> {
 
-	private class Node<E> {
+    public static void main(String[] args) {
+        CircularlyLinkedList<Integer> ll = new CircularlyLinkedList<Integer>();
+        for (int i = 10; i < 20; ++i) {
+            ll.addLast(i);
+        }
 
-	}
+        System.out.println(ll);
 
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+        ll.removeFirst();
+        System.out.println(ll);
 
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+        ll.removeLast();
 
-	@Override
-	public E get(int i) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        ll.rotate();
+        System.out.println(ll);
 
-	@Override
-	public void add(int i, E e) {
-		// TODO Auto-generated method stub
+        ll.removeFirst();
+        ll.rotate();
+        System.out.println(ll);
 
-	}
+        ll.removeLast();
+        ll.rotate();
+        System.out.println(ll);
 
-	@Override
-	public E remove(int i) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        for (Integer e : ll) {
+            System.out.println("value: " + e);
+        }
 
-	@Override
-	public E removeFirst() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    }
 
-	@Override
-	public E removeLast() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public void addFirst(E e) {
-		// TODO Auto-generated method stub
+    @Override
+    public E get(int i) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	}
+    @Override
+    public void add(int i, E e) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void addLast(E e) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public E remove(int i) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	public void rotate() {
-				
-	}
-	
-	
-	public static void main(String[] args) {
-		CircularlyLinkedList<Integer> ll = new CircularlyLinkedList<Integer>();
-		for(int i = 10; i < 20; ++i) {
-			ll.addLast(i);
-		}
+    @Override
+    public E removeFirst() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-		System.out.println(ll);
+    @Override
+    public E removeLast() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-		ll.removeFirst();
-		System.out.println(ll);
+    @Override
+    public Iterator<E> iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-		ll.removeLast();
+    @Override
+    public void addFirst(E e) {
+        // TODO Auto-generated method stub
 
-		ll.rotate();
-		System.out.println(ll);
+    }
 
-		ll.removeFirst();
-		ll.rotate();
-		System.out.println(ll);
+    @Override
+    public void addLast(E e) {
+        // TODO Auto-generated method stub
 
-		ll.removeLast();
-		ll.rotate();
-		System.out.println(ll);
+    }
 
-		for (Integer e : ll) {
-			System.out.println("value: " + e);
-		}
+    public void rotate() {
 
-	}
+    }
+
+    private class Node<E> {
+
+    }
 }
--- src/projectCode20280/SinglyLinkedList.java
+++ src/projectCode20280/SinglyLinkedList.java
@@ -1,97 +1,126 @@
 package projectCode20280;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class SinglyLinkedList<E> implements List<E> {
+    private int size;
+    private Node<E> head;
 
-	private class Node<E> {
-		/// TODO
-	}
-	
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+    public SinglyLinkedList() {
+    	size = 0;
+    	head = null;
 	}
 
-	@Override
-	public E get(int i) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public static void main(String[] args) {
+        String[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
-	@Override
-	public void add(int i, E e) {
-		// TODO Auto-generated method stub
+        SinglyLinkedList<String> sll = new SinglyLinkedList<String>();
+        for (String s : alphabet) {
+            sll.addFirst(s);
+            sll.addLast(s);
+        }
+        System.out.println(sll.toString());
 
-	}
+        sll.removeFirst();
+        System.out.println(sll.toString());
 
-	@Override
-	public E remove(int i) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        sll.removeLast();
+        System.out.println(sll.toString());
 
-	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        sll.remove(2);
+        System.out.println(sll.toString());
 
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}	
-	
+        for (String s : sll) {
+            System.out.print(s + ", ");
+        }
+    }
 
-	@Override
-	public E removeFirst() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
-	@Override
-	public E removeLast() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void addFirst(E e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addLast(E e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public static void main(String[] args) {
-		String[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-
-		SinglyLinkedList<String> sll = new SinglyLinkedList<String>();
-		for (String s : alphabet) {
-			sll.addFirst(s);
-			sll.addLast(s);
+    @Override
+    public E get(int i) {
+        if (i < 0 || i >= size) {
+        	throw new IndexOutOfBoundsException();
 		}
-		System.out.println(sll.toString());
-
-		sll.removeFirst();
-		System.out.println(sll.toString());
-		
-		sll.removeLast();
-		System.out.println(sll.toString());
-
-		sll.remove(2);
-		System.out.println(sll.toString());
-		
-		for (String s : sll) {
-			System.out.print(s + ", ");
+        Node<E> pointer = head;
+        for (int j = 0; j < i; j++) {
+        	pointer = pointer.next;
 		}
-	}
+        return pointer.data;
+    }
+
+    @Override
+    public void add(int i, E e) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public E remove(int i) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+
+    @Override
+    public E removeFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        E temp = head.data;
+        head = head.next;
+        size--;
+        return temp;
+    }
+
+    @Override
+    public E removeLast() {
+        //TODO:
+        return null;
+    }
+
+    @Override
+    public void addFirst(E e) {
+       head = new Node<E>(e, head);
+       size++;
+    }
+
+    @Override
+    public void addLast(E e) {
+       if (head == null) {
+       		head = new Node<E>(e, null);
+	   }
+       Node<E> pointer = head;
+       while (pointer.next != null) {
+       		pointer = pointer.next;
+	   }
+       pointer.next = new Node<E>(e, null);
+       size++;
+    }
+
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
 
 
 }
